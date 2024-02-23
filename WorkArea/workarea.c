547d0fd770c3b1ae91f463e959a9ced271b16f38
wily-coyote <24258996+wily-coyote@users.noreply.github.com>
2024-02-23 01:49:46 -0500

    add SPIF_SENDWININICHANGE again
    it seems to be very finnicky on my end

--- WorkArea/workarea.c
+++ WorkArea/workarea.c
@@ -32,6 +32,7 @@ int main(int argc, char* argv[]){
 		workArea.left = monitorArea.left + abs((int)(sidebarWidth*scaleBy));
 	}
 
+	SystemParametersInfo(SPI_SETWORKAREA, 0, &workArea, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
 	// For some reason, SPIF_SENDWININICHANGE resets the work area
 	// The user has to re-maximize their windows after changing the work area
 	SystemParametersInfo(SPI_SETWORKAREA, 0, &workArea, SPIF_UPDATEINIFILE);
